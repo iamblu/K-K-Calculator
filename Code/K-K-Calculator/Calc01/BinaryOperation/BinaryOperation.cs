c1d9dccb6ce2768901f6f6614ee74e564335272e
Gilttd <422944772@qq.com>
2019-08-06 17:52:49 +0800

    Update at 8-6

---
+++ Code/K-K-Calculator/Calc01/BinaryOperation/BinaryOperation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calc01
+{
+    class BinaryOperation
+    {
+        private double NumberA;
+        private double NumberB;
+
+        public double NumberA1 { get => NumberA; set => NumberA = value; }
+        public double NumberB1 { get => NumberB; set => NumberB = value; }
+
+        public void GetResult() { }
+        
+    }
+}
---
+++ Code/K-K-Calculator/Calc01/UnaryOperation/UnaryFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Operation.UnaryOperation
+{
+    class UnaryFactory
+    {
+        public void CreateUnaryOperation(string opt) {
+
+
+        }
+
+        public bool IsFirstOperation(string opt) {
+            return false;
+        }
+    }
+}
